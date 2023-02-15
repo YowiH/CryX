d3d55e6707484fdc680e4ab374b5bca4904596d2
Youssef <118484419+YowiH@users.noreply.github.com>
2023-02-15 23:01:34 +0100

    Beta

--- cryX.py
+++ cryX.py
@@ -1,42 +1,30 @@
 import string
 
-selection = input("C or U? ")
+def cryx(text, shift, alphabets):
 
-if selection == "C" :
-        
+    def shift_alphabet(alphabet):
+        return alphabet[shift:] + alphabet[:shift]
+
+    shifted_alphabets = tuple(map(shift_alphabet, alphabets))
+
+    final_alphabet = ''.join(alphabets)
+    final_shifted_alphabet = ''.join(shifted_alphabets)
+    table = str.maketrans(final_alphabet ,final_shifted_alphabet)
+    return text.translate(table)
+
+selection = input("C or U ? ")
+
+if selection == "C":
     plain_text = input("Text: ")
-
     shift = input("Shift: ")
     shift = int(shift) % 26
 
-    # THE VERSION 1 OF CryX ONLY SUPPORTS LOWERCASE CHARACTERS
-    alphabet = string.ascii_lowercase
+    print(cryx(plain_text, shift, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]))
 
-    # ABCDEFGHIJKLMNOPQRSTUVWXYZ
-
-    shifted = alphabet[shift:] + alphabet[:shift]
-
-    # HIJKLMNOPQRSTUVWXYZ + ABCDEFG = HIJKLMNOPQRSTUVWXYZABCDEFG
-
-    table = str.maketrans(alphabet, shifted)
-
-    encrypted = plain_text.translate(table) 
-
-    print(encrypted)
-
-if selection == "U" :
-
+if selection == "U":
     encrypted_text = input("Encrypted Text: ")
-
     shift = input("Shift: ")
     shift = 26 - int(shift)
     shift = shift % 26
 
-    alphabet = string.ascii_lowercase
-    shifted = alphabet[shift:] + alphabet[:shift]
-
-    table = str.maketrans(alphabet, shifted)
-
-    uncrypted = encrypted_text.translate(table) 
-
-    print(uncrypted)
+    print(cryx(encrypted_text, shift, [string.ascii_lowercase, string.ascii_uppercase, string.punctuation]))
